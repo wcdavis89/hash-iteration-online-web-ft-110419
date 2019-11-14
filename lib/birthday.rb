eafa4ad19cf1c19a6a8842349bc77778a330104e
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 06:26:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,10 +6,14 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name,age|
+     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name,age|
    if age<=12
      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
    end
   end
 end
-
-
