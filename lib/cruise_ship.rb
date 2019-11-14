de298f168bc9b5434eadaec85016bfe5990c432a
Whittney Davis <wcdavis89@yahoo.com>
2019-11-14 06:12:29 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   winner=""
   passengers.each do |suite, name|
-    if suite ++ :suite_a &&
+    if suite == :suite_a &&
       name.start_with("A")
       winner=name
     end
